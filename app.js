393a0baea037d259762e52fbd06555ae1c96c64f
Collin <collinhallett@gmail.com>
2021-08-14 19:42:23 -0500

    modularized code. User input now generates web page

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+// // const printProfileData = (profileDataArr) => {
+// //     profileDataArr.forEach(profileItem => console.log(profileItem));
+// // }
+
+// printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err; 
+
+    console.log('Porfolio complete! Check out index.html to see the output!');
+});
